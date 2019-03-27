4e4e9e26ad1b2766644b4ed8fe580b9411e7cba9
pkrueger <philip.krueger@caci.com>
2019-03-26 20:44:01 -0700

    first commit

---
+++ CircleProg/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+    class Circle : Point
+    {
+        private double radius;
+        public Circle(double xV, double yV, double r)
+        {
+            this.x = xV;
+            this.y = yV;
+            this.radius = r;
+        }
+        public void setRadius(double r)
+        {
+            radius = r > 0 ? radius : 0;
+        }
+
+        public bool onCircle(Point p)
+        {
+            return dist(p) == radius;
+        }
+
+        public bool intersect (Circle c)
+        {
+            Point p = new Point (), myCenter = new Point();
+            p.setPoint (c.x, c.y);
+            myCenter.setPoint (x, y);
+           
+            return myCenter.dist(p) < radius + c.radius;
+        }
+        public void print()
+        {
+            Console.Write("Circle: ({0}, {1}, {2})", x, y, radius);
+        }
+    }
+
